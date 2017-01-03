28a303ba044ebf45c612958cb167ca6ec0eff200
DANIEL C M RESENDE <danielcmresende@Mac-mini-de-DANIEL.local>
2017-01-03 12:51:16 -0200

    new file:   SublimeText/.sublime_profile 	new dir:    SublimeText/Installed Packages/ 	modified:   SublimeText/Preferences.sublime-settings 	modified:   bashfiles/.bash_profile 	new file:   gitfiles/.git_profile 	modified:   gitfiles/.gitconfig 	new file:   gitfiles/.gitignore 	modified:   install_profile.sh - corrects errors

--- install_profile.sh
+++ install_profile.sh
@@ -6,8 +6,8 @@ mv $ABSOLUTE_PATH $HOME
 
 # Creates simlinks
 for file in bashrc bash_profile; do
-	if [ -r "$HOME/dotfiles/bashfiles/.$file" ] && [ -f "$HOME/dotfiles/bashfiles/.$file" ]; then
-		if [ -e "$HOME/.$file"]; then
+	if [ -r "$HOME/dotfiles/bashfiles/.$file" ] && [ -f "$HOME/dotfiles/bashfiles/.$file" ] ; then
+		if [ -e "$HOME/.$file" ]; then
 			rm -rf "$HOME/.$file"
 		fi;
 		ln -s "$HOME/dotfiles/bashfiles/.$file" "$HOME/.$file";
@@ -15,18 +15,5 @@ for file in bashrc bash_profile; do
 done;
 unset file;
 
-# Creates simlink to Sublime Text preferences
-$sublimefile="$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings";
-if [ -e "$sublimefile"]; then
-	rm -rf "$sublimefile";
-fi;
-
-ln -s "$HOME/dotfiles/SublimeText/Preferences.sublime-settings" "$sublimefile";
-
-# Creates simlink to .gitconfig
-$gitfile="$HOME/.gitconfig";
-if [ -e "$gitfile"]; then
-	rm -rf "$gitfile";
-fi;
-
-ln -s "$HOME/dotfiles/gitfiles/.gitconfig" "$gitfile";
+source $ABSOLUTE_PATH/SublimeText/.sublime_profile;
+source $ABSOLUTE_PATH/gitfiles/.git_profile;
